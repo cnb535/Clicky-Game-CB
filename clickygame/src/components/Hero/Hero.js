25a323257961b162481bb62347bdd24064e42cd7
Christina Bertram <Jamerican92grl@MacBook-Pro.home>
2018-04-14 01:30:29 -0400

    React Wrapper Done

---
+++ clickygame/src/components/Hero/Hero.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Hero = () => (
+	<div className="hero">
+		<h1>Clicky Game!</h1>
+		<p>Click on an image to earn points, but don't click on any more than once!</p>
+	</div>
+);
+
+export default Hero;
